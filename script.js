48f77953fa03b05c2c232626feb1e64ec0d69937
srcxrr <161270219+srcxrr@users.noreply.github.com>
2024-08-05 10:04:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -118,9 +118,9 @@ $(document).ready(function () {
     </head>
     <body>
         <div class="container mt-1">
-            <h3 class="text-center mb-0" id="savePdfButton">LOOKMAN READYMADE CENTRE</h3>
+            <h3 class="text-center mb-0" id="savePdfButton">JAWAID DRESSES</h3>
             <p class="text-center mb-0" >TELHATTA BAZAR, SIWAN</p>
-            <p class="text-center mt-0">PNo. 7009875235</p>
+            <p class="text-center mt-0">PNo. 7870123786</p>
             <hr style="border: none; border-top: 1px dotted #000; width: 100%;" />
             <p class="mb-0"><strong>Bill To: </strong> ${customerName}</p>
             <div style="display: flex; justify-content: space-between; align-items: center;">
